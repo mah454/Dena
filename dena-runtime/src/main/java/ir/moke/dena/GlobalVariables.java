3a43947ccc58ec2ae767ab18f08502547ca5eceb
mah454 <mah454@gmail.com>
2026-04-05 20:51:45 +0330

    cleanUp

--- dena-runtime/src/main/java/ir/moke/dena/GlobalVariables.java
+++ dena-runtime/src/main/java/ir/moke/dena/GlobalVariables.java
@@ -4,6 +4,5 @@ import java.nio.file.Path;
 
 public interface GlobalVariables {
     Path denaWorkingDirectory = Path.of(System.getProperty("dena.work-dir"));
-    Path denaSharedDirectory = denaWorkingDirectory.resolve("shared");
     Path denaModulesDirectory = denaWorkingDirectory.resolve("modules");
 }
