c3c03b8411af1467d198c4615d61f9157a9dd045
mah454 <mah454@gmail.com>
2026-04-07 10:00:17 +0330

    fix print logo

--- dena-runtime/src/main/java/ir/moke/dena/utils/Logo.java
+++ dena-runtime/src/main/java/ir/moke/dena/utils/Logo.java
@@ -1,16 +1,24 @@
 package ir.moke.dena.utils;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.stream.Stream;
 
 public class Logo {
     public static void print() {
-        try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("logo")) {
-            if (inputStream != null) {
-                byte[] bytes = inputStream.readAllBytes();
-                System.out.println(new String(bytes));
-            }
-        } catch (IOException e) {
+        try (Stream<URL> stream = Thread.currentThread().getContextClassLoader().resources("logo")) {
+            stream.filter(item -> item.getFile().contains("dena-runtime"))
+                    .findFirst()
+                    .ifPresent(Logo::print);
+        }
+    }
+
+    private static void print(URL url) {
+        try (InputStream inputStream = url.openStream()) {
+            byte[] bytes = inputStream.readAllBytes();
+            System.out.write(bytes);
+            System.out.flush();
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
