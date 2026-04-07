45314b16da017fe2d156d6b426bc710f4feaf17d
mah454 <mah454@gmail.com>
2026-04-07 09:33:36 +0330

    change logo file name

--- dena-runtime/src/main/java/ir/moke/dena/utils/Logo.java
+++ dena-runtime/src/main/java/ir/moke/dena/utils/Logo.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 
 public class Logo {
     public static void print() {
-        try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("dena.logo")) {
+        try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("logo")) {
             if (inputStream != null) {
                 byte[] bytes = inputStream.readAllBytes();
                 System.out.println(new String(bytes));
